aa111f66bcf4d4743ba1d806c5c0e6cdf2308b3d
Shramistha Baranwal <60122684+ShramisthaB@users.noreply.github.com>
2022-02-15 23:13:58 +0530

    Update powerfinder.java

--- src/main/java/powerpackage/powerfinder.java
+++ src/main/java/powerpackage/powerfinder.java
@@ -1,11 +1,11 @@
 package powerpackage;
 
-public class powerfinder {
-    public static int OF(int xyz, int pqr) {
-        int p = 1;
-        for (int i = 0; i<pqr; i++) {
-            p *= xyz;
+public class Power {
+    public static int powerFinder(int base, int exponent) {
+        int output = 1;
+        for (int i = 0; i<exponent; i++) {
+            output *= base;
         }
-        return p;
+        return output;
     }
 }
